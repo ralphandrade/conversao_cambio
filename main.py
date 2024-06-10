839fe84a8b965089e7ba1d849a201736fd72f3d1
ralphandrade <ralphandrade06@gmail.com>
2024-06-10 20:26:46 -0300

    conversao_cambio

---
+++ main.py
@@ -0,0 +1,14 @@
+from forex_python.converter import CurrencyRates
+
+valor = str(input('Informe o valor da moeda a ser convertida: '))
+valor = float(valor.replace(',', '.'))
+moeda_origem = input('Informe a moeda de origem: ').upper()
+moeda_destino = input('Informe a moeda de destino: ').upper()
+
+# faz a conversão de acordo com a taxa de câmbio do dia
+result = CurrencyRates().convert(moeda_origem, moeda_destino, valor)
+
+#exibe o resultado na tela
+print(f'$ {valor:,.2f} {moeda_origem} = $ {result:,.2f} {moeda_destino}.')
+
+
